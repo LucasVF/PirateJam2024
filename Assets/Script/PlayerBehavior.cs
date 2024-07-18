f035a87cd60e0b049ecc9bc18ff0ad8d020055f3
Felipe Brum de Araujo <felipebda@hotmail.com>
2024-07-18 10:18:38 -0300

    Creation and implementation of movement of the PlayerTop gameobject.

---
+++ Assets/Script/PlayerBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public int speedMovement = 5;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0.0f);
+        rb.velocity = movement * speedMovement * Time.deltaTime;
+    }
+}
